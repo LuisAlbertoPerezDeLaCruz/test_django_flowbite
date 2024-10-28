0e424ffd1faeee531fa408ee311f484d3d8738ca
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-10-28 13:53:23 -0400

    avance

--- myapp/boards/views.py
+++ myapp/boards/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from boards.models import Board
 
 
 def home(request):
     return render(request, 'home.html', {'boards': Board.objects.all()})
+
+
+def board_topics(request, id):
+    board = Board.objects.get(id=id)
+    return render(request, 'topics.html', {'board': board})
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import index
-from boards.views import home
+from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('boards/<int:id>/', views.board_topics, name='board_topics'),
     path('index', index, name='index'),
-    path('', home, name='home'),
 ]
